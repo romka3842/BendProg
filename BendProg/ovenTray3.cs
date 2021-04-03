6d48f997e5318de86302a276e3a8d84699733814
RomanPeshaev <romka3842@gmail.com>
2021-04-03 13:25:27 +0300

    Additional changes

--- BendProg/ovenTray3.cs
+++ BendProg/ovenTray3.cs
@@ -14,7 +14,17 @@ namespace BendProg
         }
         public override double reamerA_Calculation(double kfactor)
         {
-            return base.reamerA_Calculation(kfactor);
+            if (_angle <= 90)
+            {
+                _BA = BA_calculation_before90(kfactor);
+            }
+            if (_angle > 90 && _angle < 180)
+            {
+                _BA = BA_calculation_after90(kfactor);
+            }
+
+            _reamerA = _sideA - _s + _board + _kink + _BA;
+            return _reamerA;
         }
 
         public override double reamerB_Calculation(double kfactor)
